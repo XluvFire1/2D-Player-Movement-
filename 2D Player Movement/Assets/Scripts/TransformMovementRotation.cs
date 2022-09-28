7a8c81dcb294df64a73aea50fa380940d8b562e2
Jacob Zambrano <jacobzambrano858@riversideunified.org>
2022-09-28 11:03:15 -0700

    fixing scene

--- 2D Player Movement/Assets/Scripts/TransformMovementRotation.cs
+++ 2D Player Movement/Assets/Scripts/TransformMovementRotation.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TransformMovementRotation : MonoBehaviour
 {
+    public float speed = 5;
+    public float rotationSpeed = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class TransformMovementRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+    float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+    transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+    transform.Rotate(Vector3.forward, verticalInput * rotationSpeed * Time.deltaTime);
+   }
 }
